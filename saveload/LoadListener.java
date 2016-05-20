fbe709c2779a4ff5f57bccfd94b64e245e365cda
ajames08 <ajames08@calpoly.edu>
2016-05-19 17:02:25 -0700

    556272 easy save functionality for buttons

---
+++ saveload/LoadListener.java
@@ -0,0 +1,46 @@
+package saveload;
+
+import java.awt.event.ActionEvent;
+
+/**
+ * A listener for loading the game.
+ * 
+ * @author Alec James
+ */
+public class LoadListener
+{
+    /** the game state. Set after the action completes. */
+    private GameState state;
+    
+    /** the FileLoaderWindow. */
+    private FileLoaderWindow window;
+    
+    /**
+     * Constructor. Sets the Game state.
+     * 
+     */
+    public LoadListener(String user, String pass)
+    {
+        this.state = state;
+        window = new FileLoaderWindow(user, pass);
+    }
+    
+    /**
+     * goes through a load operation. After this is called the loaded game state
+     * is accessible through getLoadedState().
+     * @param evt unused
+     */
+    public void actionPerformed(ActionEvent evt)
+    {
+        window.load();
+    }
+    
+    /**
+     * Accesses the game state after a load.
+     * @return the game state
+     */
+    public GameState getLoadedState()
+    {
+        return state;
+    }
+}
