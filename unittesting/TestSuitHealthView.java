35d265c6f09e40a9c823fd1ad81255a568e1cf66
juancafe4 <juancafe2@gmail.com>
2016-05-31 12:23:56 -0700

    541359 added a test suite

---
+++ unittesting/TestSuitHealthView.java
@@ -0,0 +1,14 @@
+package unittesting;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/*
+ * Test suite for running the HealthView unit tests.
+ * @author Juan Carlos Ferrel
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ TestHealthView.class })
+  public class TestSuiteUnit {
+  }
